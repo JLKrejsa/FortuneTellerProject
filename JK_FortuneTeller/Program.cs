b3c7c3b8af19801351d74f35d22bc6afafe4842e
Jennifer Krejsa <jlkrejsa@gmail.com>
2017-01-07 16:29:00 -0500

    second commit

--- JK_FortuneTeller/Program.cs
+++ JK_FortuneTeller/Program.cs
@@ -12,10 +12,26 @@ namespace JK_FortuneTeller
         {
             Console.WriteLine("Welcome to Madame Jen's!");
             Console.WriteLine("The answer to the mysteries of your future awaits.");
-                               "Come closer."
-                               "Ok, that's close enough."
-                               "I need to gather some information, then your future will be revealed.");
-            //Console.WriteLine();
+            Console.WriteLine("Come closer.");
+            Console.WriteLine("Ok, that's close enough.");
+            Console.WriteLine("I need to gather some information, then your future will be revealed.");
+            Console.WriteLine("What is your first name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            int age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born?");
+            int month = Console.ReadLine();
+
+            Console.WriteLine("What is your favority ROYGBIV color?");
+            Console.WriteLine("Type Help if you don't know what ROYGBIV is");
+
+
+
+            //Console.WriteLine((name ) + (lastName));
 
         }
     }
