b29e9315e6cc6e6a72df65bf04ca9e2ed81da0e0
Jennifer Krejsa <jlkrejsa@gmail.com>
2017-01-07 11:36:27 -0500

    first commit

---
+++ JK_FortuneTeller/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JK_FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Madame Jen's!");
+            Console.WriteLine("The answer to the mysteries of your future awaits.");
+                               "Come closer."
+                               "Ok, that's close enough."
+                               "I need to gather some information, then your future will be revealed.");
+            //Console.WriteLine();
+
+        }
+    }
+}
